55c1a4cd3bb406a3f71fba232be981c620ae47df
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2020-05-16 16:35:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Main from "../components/Main"
-import "../components/style.css"
+// import "../components/style.css"
 
 import Image from "../components/image"
 import SEO from "../components/seo"
