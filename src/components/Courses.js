24f611dc3fd093cf3c0ee53df5c238d059129e1b
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2020-05-16 17:40:31 +0100

    added content to courses section

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React from "react"
 
 export default function Courses() {
-    return (
-       <section id= "courses">
-            <div className="container course-row">
-            <div className="course-left">
-                <div className="course-text">
-                    <h2>Browse our Top <br/> Courses</h2>
-                    
-                </div>
-            </div>
-            <div className="course-right">
-                <img src="Images/course.png"/>
-            </div>
+  return (
+    <section id="courses">
+      <div className="container course-row">
+        <div className="course-left">
+          <div className="course-text">
+            <h2>
+              Browse our Top <br /> Courses
+            </h2>
+            <span className="square"></span>
 
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text, and a search for 'lorem ipsum' will
+              uncover many web sites still in their infancy. Various versions
+              have evolved over the years, sometimes by accident, sometimes on
+              purpose (injected humour and the like).
+            </p>
+            <button className="common-btn">View All Courses</button>
+
+            <div className="line">
+              <span className="line-1"></span>
+              <br />
+              <span className="line-2"></span>
+              <br />
+
+              <span className="line-3"></span>
             </div>
-        )
-       </section>
+          </div>
+        </div>
+        <div className="course-right">
+          <img src="Images/course.png" />
+        </div>
+      </div>
+    </section>
+  )
 }
